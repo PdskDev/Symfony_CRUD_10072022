0d0765768a90a12829f4b2efee692ebebad27fcb
PdskDev <prestadesk@gmail.com>
2022-07-11 01:32:57 +0200

    Controller, Enity, Repository, Form created

---
+++ src/Controller/MainController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Crud;
+use App\Form\CrudType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="main")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+   /**
+    * @Route("create", name="create")
+    */
+    public function create(Request $request){
+
+    $crud = new Crud();
+    $form = $this->createForm(CrudType::class, $crud);
+    $form->handleRequest($request);
+    
+    //if ($form->isSubmitted() && $form->isValid()) { 
+    //}
+
+    return $this->render("main/create.html.twig", [
+        'form' => $form->createView()
+    ]);
+    
+
+
+    }
+}
